aa7eb47f807e4e83fbc6bcdf2030555a400835b8
Anne Machinda <annemachinda@gmail.com>
2021-12-25 14:55:24 +0100

    relationship

--- models/base_model.py
+++ models/base_model.py
@@ -1,38 +1,50 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
 
 
 class BaseModel:
+    """ Classes will inherit from BaseModel to get common values(id, created_at, updated_at)"""
+    id = Column(String(60), unique = True, nullable = False, primary_key = True)
+    created_at = Column(DateTime, nullable = False, default = datetime.utcnow())
+    updated_at = Column(DateTime, nullable = False, default = datetime.utnow())
+
+    
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs.keys():
+                setattr(self, 'id', str(uuid.uuid4()))
+                self.created_at = self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
+    
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
+        if not self.created_at:
+            self.created_at = self.updated_at
+        models.storage.new(self)
+        models.storage.save()
 
+        
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
@@ -42,3 +54,9 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+
+    def delete(self):
+        """ deletes current instant for storage"""
+        from models import storage
+        storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import ForeignKey, Column, Integer, String
+from sqlalchemy.orm import relationship
+import os
 
 
 class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+    """ connecting city to the database table"""
+    __tablename__ = "cities"
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable = False)
+        state_id = Column(String(60), ForeingKey("states.id"), nullable = False)
+        places = relationship("Place", passive_deletes = True, backref = "cities")
