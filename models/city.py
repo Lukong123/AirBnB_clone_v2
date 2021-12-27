86ee1a221b3c74c455593809a9a127349b8e2318
andrew21-mch <nfonandrew73@gmail.com>
2021-12-27 21:09:55 +0100

    reviews

--- models/city.py
+++ models/city.py
@@ -3,15 +3,11 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import ForeignKey, Column, Integer, String
 from sqlalchemy.orm import relationship
-import os
-
 
 class City(BaseModel):
 
     """ connecting city to the database table"""
     __tablename__ = "cities"
-
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable = False)
-        state_id = Column(String(60), ForeingKey("states.id"), nullable = False)
-        places = relationship("Place", passive_deletes = True, backref = "cities")
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
